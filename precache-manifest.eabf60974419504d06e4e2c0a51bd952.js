484e86a3726cf0f50cd97cfb8046f762b5d56715
Anya <nancyt33333@gmail.com>
2019-08-31 12:17:16 +0300

    Updates

---
+++ precache-manifest.eabf60974419504d06e4e2c0a51bd952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528ee00300588398efc3ed064bc379ff",
+    "url": "/deploy-react-to-pages/index.html"
+  },
+  {
+    "revision": "1a1af2ce4ae738f6e44c",
+    "url": "/deploy-react-to-pages/static/css/main.be695a28.chunk.css"
+  },
+  {
+    "revision": "f478c3e6a5a59dee73f6",
+    "url": "/deploy-react-to-pages/static/js/2.81f98875.chunk.js"
+  },
+  {
+    "revision": "1a1af2ce4ae738f6e44c",
+    "url": "/deploy-react-to-pages/static/js/main.0c2f52f3.chunk.js"
+  },
+  {
+    "revision": "9da3d83b960b6853f892",
+    "url": "/deploy-react-to-pages/static/js/runtime~main.b665ec81.js"
+  }
+]);
